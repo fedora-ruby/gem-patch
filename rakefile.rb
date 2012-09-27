9b1bc2ca1a5c516132ced875910d4626381ec522
Josef Strzibny <strzibny@dhcp-25-242.brq.redhat.com>
2012-09-27 13:29:57 +0200

    Update rakefile.rb Update description

--- rakefile.rb
+++ rakefile.rb
@@ -6,11 +6,14 @@ gemspec = Gem::Specification.new do |s|
   s.version  = "0.0.1"
   s.platform = Gem::Platform::RUBY
   s.summary     = "RubyGems plugin for patching gems."
-  s.description = ""
-  s.homepage = "https://github.com/strzibny/gem-patch"
+  s.description = <<-EOF
+                    `gem-patch` is a RubyGems plugin that helpes to patch gems without manually opening and rebuilding them.
+                    It openes a given .gem file, extracts it, patches it with system "patch" command,
+                    clones its spec, updates the file list and builds the patched gem.
+                  EOF
   s.licenses = [""]
-  s.author   = ""
-  s.email    = ""
+  s.author   = "Josef Stribny"
+  s.email    = "jstribny@redhat.com"
   s.required_ruby_version     = ">= 1.8.7"
   s.required_rubygems_version = ">= 2.0.a"
   #s.add_development_dependency 'rake', '~> 0.9.2.2'
