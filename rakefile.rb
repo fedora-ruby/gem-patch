8156ea712ad96264c6df2767137352ba02e81182
Josef Stribny <jstribny@redhat.com>
2014-07-07 14:51:51 +0200

    Bump version to 0.1.6

--- rakefile.rb
+++ rakefile.rb
@@ -3,31 +3,31 @@ require 'rake/testtask'
 require 'rdoc/task'
 
 gemspec = Gem::Specification.new do |s|
-  s.name     = "gem-patch"
-  s.version  = "0.1.5"
+  s.name     = 'gem-patch'
+  s.version  = '0.1.6'
   s.platform = Gem::Platform::RUBY
-  s.summary     = "RubyGems plugin for patching gems."
+  s.summary     = 'RubyGems plugin for patching gems.'
   s.description = <<-EOF
-                    `gem-patch` is a RubyGems plugin that helps to patch gems without manually opening and rebuilding them.
-                    It opens a given .gem file, extracts it, patches it with system `patch` command,
+                    gem-patch is a RubyGems plugin that helps to patch gems without manually opening and rebuilding them.
+                    It opens a given .gem file, extracts it, patches it with system patch command,
                     clones its spec, updates the file list and builds the patched gem.
                   EOF
-  s.homepage = "http://github.com/strzibny/gem-patch"
-  s.licenses = ["MIT"]
-  s.author   = "Josef Stribny"
-  s.email    = "jstribny@redhat.com"
-  s.required_ruby_version     = ">= 1.8.7"
-  s.required_rubygems_version = ">= 1.8.0"
-  s.files = FileList["README.md", "LICENCE", "rakefile.rb",
-                      "lib/**/*.rb", "test/**/test*.rb"]
+  s.homepage = 'http://github.com/strzibny/gem-patch'
+  s.licenses = ['MIT']
+  s.author   = 'Josef Stribny'
+  s.email    = 'jstribny@redhat.com'
+  s.required_ruby_version     = '>= 1.8.7'
+  s.required_rubygems_version = '>= 1.8.0'
+  s.files = FileList['README.md', 'LICENCE', 'rakefile.rb',
+                     'lib/**/*.rb', 'test/**/test*.rb']
 end
 
 Gem::PackageTask.new gemspec do |pkg|
 end
 
 Rake::RDocTask.new do |rd|
-  rd.main = "README.md"
-  rd.rdoc_files.include("README.md", "lib/**/*.rb")
+  rd.main = 'README.md'
+  rd.rdoc_files.include('README.md', 'lib/**/*.rb')
 end
 
 Rake::TestTask.new('test') do |t|
